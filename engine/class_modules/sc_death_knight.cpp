c28d45b6a536897e5e6d23667f3c19f16bb9a8a3
Khazak <joe.j.sommer@gmail.com>
2025-08-21 23:32:11 -0500

    [Death Knight] Mark DSlice as unsupported for Frost

--- engine/class_modules/sc_death_knight.cpp
+++ engine/class_modules/sc_death_knight.cpp
@@ -15840,7 +15840,6 @@ bool death_knight_t::validate_fight_style( fight_style_e fight ) const
     {
       case FIGHT_STYLE_PATCHWERK:
       case FIGHT_STYLE_CASTING_PATCHWERK:
-      case FIGHT_STYLE_DUNGEON_SLICE:
       case FIGHT_STYLE_DUNGEON_ROUTE:
         return true;
       default:
